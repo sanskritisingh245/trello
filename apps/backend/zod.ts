e7af297e056d5ea65b9da66e3e9208acf6347c06
sanskritisingh245 <sanskritisinghh2005@gmail.com>
2026-08-05 01:12:45 +0530

    file-structure

--- apps/backend/zod.ts
+++ apps/backend/zod.ts
@@ -3,10 +3,49 @@ import z, { email } from "zod";
 
 export const SignupSchema = z.object({
     email: z.email(),
-    password:z.string()  
+    password:z.string(),
+    profile:z.string(),
+    description:z.string() 
 })
 
 export const OrgSchema = z.object({
     name:z.string(),
    description:z.string()
+})
+
+export const updateOrgSchema= z.object({
+    name:z.string().optional(),
+    description:z.string().optional()
+})
+
+export const BoardSchema =z.object({
+    title:z.string(),
+})
+
+export const SectionSchema= z.object({
+    title:z.string(),
+    boardId:z.string(),
+})
+
+export const SectionUpdateSchema= z.object({
+    title:z.string()
+})
+
+export const CommentsSchema = z.object({
+    comment:z.string()
+})
+
+export const IssueSchema= z.object({
+    title:z.string(),
+    description:z.string(),
+})
+
+export const UpdateIssueSchema= z.object({
+    title:z.string().optional(),
+    description:z.string().optional(),
+})
+
+export const MembershipSchema= z.object({
+    email:z.email(),
+    orgId:z.string()
 })
